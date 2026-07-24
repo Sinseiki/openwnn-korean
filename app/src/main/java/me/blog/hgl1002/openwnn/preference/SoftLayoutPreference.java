919d7940589d7c8e3c3256258c1e66fcb8829f91
Lee0701 <hgl1002@naver.com>
2026-07-24 17:46:04 +0900

    Rename L3.0 to L0.9, remove from default soft layout list
    (cherry picked from commit 1ae1c3b007ffa80b7d85354b0a509c7de0fb3b99)

--- app/src/main/java/me/blog/hgl1002/openwnn/preference/SoftLayoutPreference.java
+++ app/src/main/java/me/blog/hgl1002/openwnn/preference/SoftLayoutPreference.java
@@ -75,7 +75,7 @@ public class SoftLayoutPreference extends ListPreference {
 			return R.array.keyboard_soft_layout_l1_9;
 
 		case "keyboard_sebul_semoe":
-			return R.array.keyboard_soft_layout_l3_0;
+			return R.array.keyboard_soft_layout_l0_9;
 
 		case "keyboard_alphabet_dvorak":
 			return R.array.keyboard_soft_layout_dvorak;
@@ -121,7 +121,7 @@ public class SoftLayoutPreference extends ListPreference {
 			return R.array.keyboard_soft_layout_l1_4_id;
 
 		case "keyboard_sebul_semoe":
-			return R.array.keyboard_soft_layout_l3_0_id;
+			return R.array.keyboard_soft_layout_l0_9_id;
 
 		case "keyboard_alphabet_colemak":
 		case "keyboard_alphabet_rose":
